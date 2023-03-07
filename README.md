2696b7661d644c2f29d7d2ef6bbd6c3523f7f597
SYEDUMAIR007 <92495743+SYEDUMAIR007@users.noreply.github.com>
2023-03-07 17:00:50 +0530

    first commt

---
+++ README.md
@@ -0,0 +1,67 @@
+# Frontend Mentor - Todo app solution
+
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+  - [Author](#author)
+  - [Acknowledgments](#acknowledgments)
+
+
+
+## Overview
+Frontend mentor is a great companion for learning web development. This challenge was TRICKY as it was my first time building a full stack app and i didnt had the figma or sketch files but im happy that i could make it as realistic as possible.
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- Drag and drop to reorder items on the list
+- store their todos in local storage
+
+
+
+## My process
+I first went on to design my own mockup of the design using figma and then built the project.
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Java script
+
+### What I learned
+I understood vanilla js very well while creating this project.
+
+### Useful resources
+
+- [Figma](https://www.figma.com) - This helped me for buildin the mockup . I really liked this pattern of building mockups first and will use it going forward.
+- [MDN web docs](https://developer.mozilla.org/en-US/) - This is an amazing website which helped me finally understand the vanilla js. 
+
+
+## Author
+
+- Frontend Mentor -[@SYEDUMAIR007](https://www.frontendmentor.io/profile/SYEDUMAIR007)
+
+
+
+## Acknowledgments
+
+Todo app (https://www.youtube.com/watch?v=Ttf3CEsEwMQ&t=3912s) by developedbyed has helped me alot while implementing features like local storage.
+(https://www.youtube.com/watch?v=jfYWwQrtzzY) by webdev simplified has helped me in implementing the drag n drop option in my app.
+
---
+++ app.js
@@ -0,0 +1,391 @@
+//declarations
+const todoMaker = document.querySelector(".todoMaker");
+const todoUl = document.querySelector(".todoUl");
+const addTodo = document.querySelector("#input");
+const itemCount = document.querySelector("#itemCount");
+const allClear = document.querySelector("#allClear");
+const blockFilters = document.querySelectorAll("#filters a");
+const inlineFilters = document.querySelectorAll("#inline-filter a");
+const modeSwitch = document.querySelector("#mode-toggler");
+const filters = document.querySelectorAll(".filters a");
+const dropContainer = document.querySelectorAll(".dragContainer");
+
+//event listeners
+document.addEventListener('DOMContentLoaded',getTodos);
+addTodo.addEventListener("keypress", todoCreator);
+todoUl.addEventListener("click", deleteNcheck);
+allClear.addEventListener("click", clearTodos);
+for (let blockFilter of blockFilters) {
+  blockFilter.addEventListener("click", displayFilters);
+}
+for (let inlineFilter of inlineFilters) {
+  inlineFilter.addEventListener("click", displayFilters);
+}
+modeSwitch.addEventListener("click", switchMode);
+
+
+//functions
+
+function todoCreator(e) {
+  if (e.key === "Enter" && addTodo.value) {
+    e.preventDefault();
+
+    const todoContainer = document.createElement("div");
+    todoContainer.classList.add("todoContainer");
+    modeSwitch.classList.contains("dark-mode") ?
+      todoContainer.classList.add("dark-mode-todo") :
+      todoContainer.classList.add("todo");
+
+    let checkboxCount = todoUl.childElementCount;
+    let checkbox= `checkbox${checkboxCount}`;
+   
+  
+    const todoCheck = document.createElement("input");
+    const label = document.createElement("label")
+    todoCheck.type = "checkbox";
+    todoCheck.id=checkbox;
+    label.setAttribute('for',checkbox);
+    todoCheck.classList.add("listCheckbox");
+    todoContainer.appendChild(todoCheck);
+    todoContainer.appendChild(label);
+    
+
+    const todo = document.createElement("li");
+    todo.innerText = addTodo.value;
+    todoContainer.appendChild(todo);
+    saveLocalTodos(addTodo.value);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("deleteBtn");
+    todoContainer.appendChild(deleteBtn);
+    todoContainer.setAttribute('draggable', 'true');
+    todoUl.prepend(todoContainer);
+    addTodo.value = "";
+    itemCount.innerText = todoUl.childElementCount - 1;
+  }
+  const draggable = document.querySelectorAll(".todoContainer");
+  draggable.forEach(todo => {
+    todo.addEventListener('dragstart', () => {
+      todo.classList.add("dragging");
+    });
+    todo.addEventListener('dragend', () => {
+      todo.classList.remove("dragging");
+    });
+  });
+
+  dropContainer.forEach(container => {
+    container.addEventListener("dragover", e => {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(container, e.clientY);
+      let droppable = document.querySelector(".dragging");
+      if (afterElement == null) {
+        droppable.classList.add("no-style")
+        container.prepend(droppable)
+      } else {
+        container.insertBefore(droppable, afterElement)
+      }
+    })
+  })
+
+}
+
+function deleteNcheck(e) {
+  const item = e.target;
+  if (item.classList.contains("deleteBtn")) {
+    const deleteable=item.parentElement;
+    removeLocalTodos(item.parentElement);
+    deleteable.remove();
+    itemCount.innerText = todoUl.childElementCount - 1;
+  }
+  if (item.classList.contains("listCheckbox")) {
+    item.parentElement.classList.toggle("completed");
+    item.classList.toggle("completedCheck");
+  }
+}
+
+function clearTodos() {
+  const checkboxes = document.querySelectorAll(".listCheckbox");
+  for (const checkbox of checkboxes) {
+    if (checkbox.checked) {
+      checkbox.parentElement.remove();
+      removeLocalTodos(checkbox.parentElement);
+    }
+  }
+  itemCount.innerText = todoUl.childElementCount - 1;
+}
+
+function displayCompleted() {
+  const checkboxes = document.querySelectorAll(".listCheckbox");
+  let count = 0;
+  for (const checkbox of checkboxes) {
+    if (checkbox.checked) {
+      checkbox.parentElement.style.display = "";
+      count++;
+    } else {
+      checkbox.parentElement.style.display = "none";
+    }
+  }
+  itemCount.innerText = count;
+}
+
+function displayAll() {
+  const checkboxes = document.querySelectorAll(".listCheckbox");
+  for (const checkbox of checkboxes) {
+    checkbox.parentElement.style.display = "";
+  }
+  itemCount.innerText = todoUl.childElementCount - 1;
+}
+
+function displayActive() {
+  const checkboxes = document.querySelectorAll(".listCheckbox");
+  let count = 0;
+  for (const checkbox of checkboxes) {
+    if (checkbox.checked === false) {
+      checkbox.parentElement.style.display = "";
+      count++;
+    } else {
+      checkbox.parentElement.style.display = "none";
+    }
+  }
+  itemCount.innerText = count;
+}
+
+function displayFilters(e) {
+  const item = e.target;
+
+  if (item.classList.contains("allFilter")) {
+
+    displayAll();
+  }
+  else if (item.classList.contains("activeFilter")) {
+
+    displayActive();
+  }
+  else if (item.classList.contains("completedFilter")) {
+
+    displayCompleted();
+  }
+
+}
+
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.todoContainer:not(.dragging)')]
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+function switchMode() {
+  let todos = document.querySelectorAll(".todoContainer");
+  let BlockFilters = document.querySelector("#filters");
+  let InlineFilters = document.querySelector(".todo:last-child");
+  modeSwitch.classList.toggle("dark-mode");
+  modeSwitch.classList.toggle("light-mode");
+  addTodo.classList.toggle("dark-mode-input");
+  todoMaker.classList.toggle("dark-mode-input");
+  InlineFilters.classList.toggle("filter-dark-mode");
+
+
+
+  switch (true) {
+    case (modeSwitch.classList.contains("dark-mode") && window.matchMedia("(max-width: 375px)").matches):
+      document.body.style.backgroundImage = "url('./images/bg-mobile-dark.jpg')";
+      document.body.style.backgroundColor = "hsl(235, 21%, 11%)";
+      BlockFilters.style.backgroundColor = "hsl(235, 24%, 19%)";
+      filters.forEach((filter) => {
+        filter.addEventListener('mouseover', () => {
+          filter.style.color = 'hsl(236, 33%, 92%)';
+        });
+
+        filter.addEventListener('mouseout', () => {
+          filter.style.color = 'hsl(236, 9%, 61%)';
+        });
+        filter.addEventListener('focus', () => {
+          filter.style.color = 'hsl(220, 98%, 61%)';
+        });
+      });
+      for (let todo of todos) {
+        todo.classList.add("dark-mode-todo");
+        todo.classList.remove("todo");
+      }
+      break;
+
+    case (modeSwitch.classList.contains("light-mode") && window.matchMedia("(max-width: 375px)").matches):
+      document.body.style.backgroundImage = "url('./images/bg-mobile-light.jpg')";
+      document.body.style.backgroundColor = "hsl(236, 33%, 92%)";
+      BlockFilters.style.backgroundColor = "hsl(0, 0%, 98%)";
+      filters.forEach((filter) => {
+        filter.addEventListener('mouseover', () => {
+          filter.style.color = 'hsl(235, 19%, 35%)';
+        });
+
+        filter.addEventListener('mouseout', () => {
+          filter.style.color = 'hsl(236, 9%, 61%)';
+        });
+        filter.addEventListener('focus', () => {
+          filter.style.color = 'hsl(220, 98%, 61%)';
+        });
+      });
+
+      for (let todo of todos) {
+        todo.classList.remove("dark-mode-todo");
+        todo.classList.add("todo");
+      }
+      break;
+
+    case (modeSwitch.classList.contains("dark-mode") && window.matchMedia("(min-width: 376px)").matches):
+      document.body.style.backgroundImage = "url('./images/bg-desktop-dark.jpg')";
+      document.body.style.backgroundColor = "hsl(235, 21%, 11%)";
+      BlockFilters.style.backgroundColor = "hsl(235, 24%, 19%)";
+      filters.forEach((filter) => {
+        filter.addEventListener('mouseover', () => {
+          filter.style.color = 'hsl(236, 33%, 92%)';
+        });
+
+        filter.addEventListener('mouseout', () => {
+          filter.style.color = 'hsl(236, 9%, 61%)';
+        });
+        filter.addEventListener('focus', () => {
+          filter.style.color = 'hsl(220, 98%, 61%)';
+        });
+      });
+      for (let todo of todos) {
+        todo.classList.add("dark-mode-todo");
+        todo.classList.remove("todo");
+      }
+      break;
+
+    case (modeSwitch.classList.contains("light-mode") && window.matchMedia("(min-width: 376px)").matches):
+      document.body.style.backgroundImage = "url('./images/bg-desktop-light.jpg')";
+      document.body.style.backgroundColor = "hsl(236, 33%, 92%)";
+      BlockFilters.style.backgroundColor = "hsl(0, 0%, 98%)";
+      filters.forEach((filter) => {
+        filter.addEventListener('mouseover', () => {
+          filter.style.color = 'hsl(235, 19%, 35%)';
+        });
+
+        filter.addEventListener('mouseout', () => {
+          filter.style.color = 'hsl(236, 9%, 61%)';
+        });
+        filter.addEventListener('focus', () => {
+          filter.style.color = 'hsl(220, 98%, 61%)';
+        });
+      });
+
+
+      for (let todo of todos) {
+        todo.classList.remove("dark-mode-todo");
+        todo.classList.add("todo");
+      }
+      break;
+
+    default:
+      break;
+  }
+
+}
+
+function saveLocalTodos(Todo){
+  let todos;
+  if(localStorage.getItem("todos")===null){ 
+    todos=[];
+  }
+  else{
+  todos=JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(Todo);
+  localStorage.setItem("todos",JSON.stringify(todos));
+}
+
+function getTodos(){
+  let todos;
+  if(localStorage.getItem("todos")===null){ 
+    todos=[];
+  }
+  else{
+  todos=JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(function(Todo){
+    const todoContainer = document.createElement("div");
+    todoContainer.classList.add("todoContainer");
+    modeSwitch.classList.contains("dark-mode") ?
+      todoContainer.classList.add("dark-mode-todo") :
+      todoContainer.classList.add("todo");
+
+    let checkboxCount = todoUl.childElementCount;
+    let checkbox= `checkbox${checkboxCount}`;
+   
+  
+    const todoCheck = document.createElement("input");
+    const label = document.createElement("label")
+    todoCheck.type = "checkbox";
+    todoCheck.id=checkbox;
+    label.setAttribute('for',checkbox);
+    todoCheck.classList.add("listCheckbox");
+    todoContainer.appendChild(todoCheck);
+    todoContainer.appendChild(label);
+    
+
+    const todo = document.createElement("li");
+    todo.innerText = Todo;
+    todoContainer.appendChild(todo);
+ 
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("deleteBtn");
+    todoContainer.appendChild(deleteBtn);
+    todoContainer.setAttribute('draggable', 'true');
+    todoUl.prepend(todoContainer);
+    addTodo.value = "";
+    itemCount.innerText = todoUl.childElementCount - 1;
+  
+  const draggable = document.querySelectorAll(".todoContainer");
+  draggable.forEach(todo => {
+    todo.addEventListener('dragstart', () => {
+      todo.classList.add("dragging");
+    });
+    todo.addEventListener('dragend', () => {
+      todo.classList.remove("dragging");
+    });
+  });
+
+  dropContainer.forEach(container => {
+    container.addEventListener("dragover", e => {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(container, e.clientY);
+      let droppable = document.querySelector(".dragging");
+      if (afterElement == null) {
+        droppable.classList.add("no-style")
+        container.prepend(droppable);
+      } else {
+        container.insertBefore(droppable, afterElement)
+      }
+    })
+  })
+
+})
+}
+function removeLocalTodos(Todo){
+  let todos;
+  if(localStorage.getItem("todos")===null){ 
+    todos=[];
+  }
+  else{
+  todos=JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex=Todo.children[2].innerText;
+  todos.splice(todos.indexOf(todoIndex),1);
+  localStorage.setItem('todos',JSON.stringify(todos));
+ 
+}
+
+
+
